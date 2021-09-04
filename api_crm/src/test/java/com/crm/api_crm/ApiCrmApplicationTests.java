c8033e3dd334187912ecda43597c3d679caaf053
kasandra rivera <kasandra.riveral@gmail.com>
2021-09-04 01:56:33 -0500

    inicio

---
+++ api_crm/src/test/java/com/crm/api_crm/ApiCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.api_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
