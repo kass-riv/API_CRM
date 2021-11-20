a59aad5325f3a465ddb50dc60efd11fc80f3e51b
Shamir Castro <shamir.castrocordova.9@hotmail.com>
2021-11-20 04:37:48 -0500

    dashboard

--- api_crm/src/main/java/com/acme/api_crm/controller/rest/DashboardController.java
+++ api_crm/src/main/java/com/acme/api_crm/controller/rest/DashboardController.java
@@ -23,7 +23,7 @@ public class DashboardController {
 
     @GetMapping(value = "/pedidostotales", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Map<String, Object>>> productos(){
-        return  new ResponseEntity<List<Map<String, Object>>>(
+        return new ResponseEntity<List<Map<String, Object>>>(
             pedidosData.querySumaTotal(), HttpStatus.OK);
     }
 
